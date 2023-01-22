e5aade585a1298c0bcbedbc939d880f2bef4d7d8
itsmashikur <itsmashikur@gmail.com>
2023-01-22 16:03:15 +0600

    Firebase Push Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'internet_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() {
+//Handle Background Notification
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+}
+
+void main() async {
+  // Firebase Push Notification
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //Handle Background Notification
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Run
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: InternetCheck(),
